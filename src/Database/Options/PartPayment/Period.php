161d14a9315db9c35240db7b64d41642521f1258
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-11-22 13:43:18 +0100

    WOO-738 Add settings for Part payment widget

---
+++ src/Database/Options/PartPayment/Period.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Woocommerce\Database\Options\PartPayment;
+
+use Resursbank\Woocommerce\Database\StringOption;
+
+/**
+ * Setting for globally enabling the gateway (not the plugin).
+ */
+class Period extends StringOption
+{
+	/**
+	 * @inheritdoc
+	 */
+	public static function getName(): string
+	{
+		return self::NAME_PREFIX . 'partpayment_period';
+	}
+
+	/**
+	 * Get a boolean value of the setting. Used internally, and not by WooCommerce that still struggles
+	 * with getting the values as yes/no.
+	 * @return bool
+	 */
+	public static function isEnabled(): bool
+	{
+		return self::getData() === 'yes';
+	}
+}
