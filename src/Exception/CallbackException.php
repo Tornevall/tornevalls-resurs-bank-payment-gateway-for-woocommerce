4e45f3e809593a48be728f5e91d2000e8c852b6d
Tomas Tornevall <thorne@tornevall.net>
2022-11-24 13:55:27 +0100

    CallbackException updated.

---
+++ src/Exception/CallbackException.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace ResursBank\Exception;
+
+use Exception;
+
+class CallbackException extends Exception
+{
+}
