86022c9f05271446bc4d5f54a1de087546c69283
Tomas Tornevall <thorne@tornevall.net>
2022-11-28 11:17:08 +0100

    Merged master with Route-fix.

--- init.php
+++ init.php
@@ -85,6 +85,4 @@ add_action('plugins_loaded', 'ResursBank\Service\WordPress::initializeWooCommerc
 // Necessary on an early level.
 add_filter('rbwc_get_custom_form_fields', 'ResursBank\Module\FormFields::getDeveloperTweaks', 10, 2);
 
-// @todo Execute custom routing options
-// @todo This still generates blank pages, so it is temporaily disabled. This has to be fixed before enabling.
-//Route::exec();
+Route::exec();
