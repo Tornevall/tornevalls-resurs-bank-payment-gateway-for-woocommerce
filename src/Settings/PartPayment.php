0c6451ce03c07ae7c25967e620c618511bbabdef
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-11-30 16:12:27 +0100

    WOO-738 Get rid of tabs

--- src/Settings/PartPayment.php
+++ src/Settings/PartPayment.php
@@ -26,6 +26,8 @@ use Resursbank\Ecom\Lib\Model\PaymentMethod;
 use Resursbank\Ecom\Lib\Order\PaymentMethod\Type;
 use Resursbank\Ecom\Module\AnnuityFactor\Models\AnnuityInformation;
 use Resursbank\Ecom\Module\PaymentMethod\Repository;
+use Resursbank\Ecom\Module\AnnuityFactor\Repository as AnnuityRepository;
+use Resursbank\Ecom\Module\PriceSignage\Repository as SignageRepository;
 use ResursBank\Service\WordPress;
 use Resursbank\Woocommerce\Database\Options\PartPayment\Enabled;
 use Resursbank\Woocommerce\Database\Options\PartPayment\PaymentMethod as PaymentMethodOption;
@@ -37,129 +39,129 @@ use Resursbank\Woocommerce\Database\Options\StoreId;
  */
 class PartPayment
 {
-	public const SECTION_ID = 'partpayment';
-	public const SECTION_TITLE = 'Part payment';
+    public const SECTION_ID = 'partpayment';
+    public const SECTION_TITLE = 'Part payment';
 
-	/**
-	 * @return array[]
-	 * @throws ApiException
-	 * @throws AuthException
-	 * @throws CacheException
-	 * @throws ConfigException
-	 * @throws CurlException
-	 * @throws EmptyValueException
-	 * @throws IllegalTypeException
-	 * @throws IllegalValueException
-	 * @throws JsonException
-	 * @throws ReflectionException
-	 * @throws ValidationException
-	 */
-	#[ArrayShape( [ self::SECTION_ID => "array" ] )] public static function getSettings(): array
-	{
-		return [
-			self::SECTION_ID => [
-				'title' => self::SECTION_TITLE,
-				'enabled' => [
-					'id' => Enabled::getName(),
-					'title' => 'Part payment widget enabled',
-					'type' => 'checkbox',
-					'default' => 'no',
-					'desc' => 'Enabled'
-				],
-				'payment_method' => [
-					'id' => PaymentMethodOption::getName(),
-					'title' => 'Payment method',
-					'type' => 'select',
-					'options' => self::getPaymentMethods()
-				],
-				'period' => [
-					'id' => Period::getName(),
-					'title' => 'Annuity period',
-					'type' => 'select',
-					'options' => self::getAnnuityPeriods()
-				]
-			]
-		];
-	}
+    /**
+     * @return array[]
+     * @throws ApiException
+     * @throws AuthException
+     * @throws CacheException
+     * @throws ConfigException
+     * @throws CurlException
+     * @throws EmptyValueException
+     * @throws IllegalTypeException
+     * @throws IllegalValueException
+     * @throws JsonException
+     * @throws ReflectionException
+     * @throws ValidationException
+     */
+    #[ArrayShape([self::SECTION_ID => "array"])] public static function getSettings(): array
+    {
+        return [
+            self::SECTION_ID => [
+                'title'          => self::SECTION_TITLE,
+                'enabled'        => [
+                    'id'      => Enabled::getName(),
+                    'title'   => 'Part payment widget enabled',
+                    'type'    => 'checkbox',
+                    'default' => 'no',
+                    'desc'    => 'Enabled'
+                ],
+                'payment_method' => [
+                    'id'      => PaymentMethodOption::getName(),
+                    'title'   => 'Payment method',
+                    'type'    => 'select',
+                    'options' => self::getPaymentMethods()
+                ],
+                'period'         => [
+                    'id'      => Period::getName(),
+                    'title'   => 'Annuity period',
+                    'type'    => 'select',
+                    'options' => self::getAnnuityPeriods()
+                ]
+            ]
+        ];
+    }
 
-	/**
-	 * Fetch available payment method options
-	 *
-	 * @return array
-	 * @throws JsonException
-	 * @throws ReflectionException
-	 * @throws ApiException
-	 * @throws AuthException
-	 * @throws CacheException
-	 * @throws ConfigException
-	 * @throws CurlException
-	 * @throws ValidationException
-	 * @throws EmptyValueException
-	 * @throws IllegalTypeException
-	 * @throws IllegalValueException
-	 */
-	private static function getPaymentMethods(): array
-	{
-		$storeId = StoreId::getData();
+    /**
+     * Fetch available payment method options
+     *
+     * @return array
+     * @throws JsonException
+     * @throws ReflectionException
+     * @throws ApiException
+     * @throws AuthException
+     * @throws CacheException
+     * @throws ConfigException
+     * @throws CurlException
+     * @throws ValidationException
+     * @throws EmptyValueException
+     * @throws IllegalTypeException
+     * @throws IllegalValueException
+     */
+    private static function getPaymentMethods(): array
+    {
+        $storeId = StoreId::getData();
 
-		$options = [];
-		try {
-			$paymentMethods = Repository::getPaymentMethods(storeId: $storeId);
-		} catch (Exception $exception) {
-			WordPress::setGenericError(exception: $exception);
-			throw $exception;
-		}
+        $options = [];
+        try {
+            $paymentMethods = Repository::getPaymentMethods(storeId: $storeId);
+        } catch (Exception $exception) {
+            WordPress::setGenericError(exception: $exception);
+            throw $exception;
+        }
 
-		/** @var PaymentMethod $paymentMethod */
-		foreach ($paymentMethods as $paymentMethod) {
-			if (
-				$paymentMethod->type === Type::RESURS_PART_PAYMENT ||
-				$paymentMethod->type === Type::RESURS_REVOLVING_CREDIT
-			) {
-				$options[$paymentMethod->id] = $paymentMethod->name;
-			}
-		}
+        /** @var PaymentMethod $paymentMethod */
+        foreach ($paymentMethods as $paymentMethod) {
+            if (
+                $paymentMethod->type === Type::RESURS_PART_PAYMENT ||
+                $paymentMethod->type === Type::RESURS_REVOLVING_CREDIT
+            ) {
+                $options[$paymentMethod->id] = $paymentMethod->name;
+            }
+        }
 
-		return $options;
-	}
+        return $options;
+    }
 
-	/**
-	 * Fetch annuity period options for configured payment method
-	 *
-	 * @return array
-	 * @throws JsonException
-	 * @throws ReflectionException
-	 * @throws ApiException
-	 * @throws AuthException
-	 * @throws CacheException
-	 * @throws ConfigException
-	 * @throws CurlException
-	 * @throws ValidationException
-	 * @throws EmptyValueException
-	 * @throws IllegalTypeException
-	 * @throws IllegalValueException
-	 */
-	private static function getAnnuityPeriods(): array
-	{
-		$paymentMethodId = PaymentMethodOption::getData();
+    /**
+     * Fetch annuity period options for configured payment method
+     *
+     * @return array
+     * @throws JsonException
+     * @throws ReflectionException
+     * @throws ApiException
+     * @throws AuthException
+     * @throws CacheException
+     * @throws ConfigException
+     * @throws CurlException
+     * @throws ValidationException
+     * @throws EmptyValueException
+     * @throws IllegalTypeException
+     * @throws IllegalValueException
+     */
+    private static function getAnnuityPeriods(): array
+    {
+        $paymentMethodId = PaymentMethodOption::getData();
 
-		try {
-			$annuityFactors = \Resursbank\Ecom\Module\AnnuityFactor\Repository::getAnnuityFactors(
-				storeId: StoreId::getData(),
-				paymentMethodId: $paymentMethodId
-			);
-		} catch (Exception $exception) {
-			WordPress::setGenericError(exception: $exception);
-			throw $exception;
-		}
+        try {
+            $annuityFactors = AnnuityRepository::getAnnuityFactors(
+                storeId: StoreId::getData(),
+                paymentMethodId: $paymentMethodId
+            );
+        } catch (Exception $exception) {
+            WordPress::setGenericError(exception: $exception);
+            throw $exception;
+        }
 
-		$return = [];
+        $return = [];
 
-		/** @var AnnuityInformation $annuityFactor */
-		foreach ($annuityFactors->content as $annuityFactor) {
-			$return[$annuityFactor->durationInMonths] = $annuityFactor->durationInMonths . ' months';
-		}
+        /** @var AnnuityInformation $annuityFactor */
+        foreach ($annuityFactors->content as $annuityFactor) {
+            $return[$annuityFactor->durationInMonths] = $annuityFactor->durationInMonths . ' months';
+        }
 
-		return $return;
-	}
+        return $return;
+    }
 }
