bbf3da0e314f78f7fc3e2b8a3d72eef0c8075d0e
Tomas Tornevall <thorne@tornevall.net>
2022-12-09 10:07:23 +0100

    throwable fix

--- src/Gateway/ResursDefault.php
+++ src/Gateway/ResursDefault.php
@@ -534,7 +534,7 @@ class ResursDefault extends WC_Payment_Gateway
 
         try {
             $sessionCustomerType = WcSession::getCustomerType();
-        } catch (Throwable $wcSessionException) {
+        } catch (Throwable) {
             // Possible to-do: Make sure that defaults are set by available payment methods, not just NATURAL.
             // Normally, this is not a problem, since the merchant majority is of type NATURAL, so for now we're
             // good to go with this.
@@ -1094,7 +1094,7 @@ class ResursDefault extends WC_Payment_Gateway
 
         try {
             $customerType = WcSession::getCustomerType();
-        } catch (Throwable $wcSessionException) {
+        } catch (Throwable) {
             // Possible to-do: Make sure that defaults are set by available payment methods, not just NATURAL.
             // Normally, this is not a problem, since the merchant majority is of type NATURAL, so for now we're
             // good to go with this.
