75d3db436bc1b492dde33708925655efa23058d9
Tomas Tornevall <tomas.tornevall@resurs.se>
2023-02-02 09:28:20 +0100

    Further cleanup (Related to WOO-990)

--- src/Module/PluginHooks.php
+++ src/Module/PluginHooks.php
@@ -1,34 +1,14 @@
 <?php
 
 /** @noinspection ParameterDefaultValueIsNotNullInspection */
-
 /** @noinspection PhpUsageOfSilenceOperatorInspection */
 
 namespace ResursBank\Module;
 
-use Automattic\WooCommerce\Admin\Overrides\Order;
-use Exception;
-use Resursbank\Ecom\Config;
-use Resursbank\Ecom\Lib\Model\PaymentMethod;
-use Resursbank\Ecom\Lib\Order\PaymentMethod\Type;
 use Resursbank\Ecommerce\Types\CheckoutType;
-use Resursbank\Woocommerce\Modules\Api\Connection;
-use Resursbank\Woocommerce\Modules\Ordermanagement\Module as OrdermanagementModule;
 use Resursbank\RBEcomPHP\ResursBank;
-use ResursBank\Service\WooCommerce;
-use ResursBank\Service\WordPress;
-use RuntimeException;
-use Throwable;
+use Resursbank\Woocommerce\Modules\Ordermanagement\Module as OrdermanagementModule;
 use TorneLIB\IO\Data\Arrays;
-use TorneLIB\Utils\WordPress as wpHelper;
-use WC_Order;
-use WC_Order_Item_Product;
-use WC_Order_Refund;
-use WC_Product;
-use WC_Tax;
-use function count;
-use function in_array;
-use function is_array;
 
 /**
  * Class Plugin Internal plugin handler.
@@ -48,5 +28,4 @@ class PluginHooks
 
         OrdermanagementModule::setupActions();
     }
-
 }
