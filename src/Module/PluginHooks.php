3a79bf8afa7fbec42de089aaf253943a504b1af4
Tomas Tornevall <tomas.tornevall@resurs.se>
2023-02-02 09:29:36 +0100

    Cleaned up further conflicts with new order management module.

--- src/Module/PluginHooks.php
+++ src/Module/PluginHooks.php
@@ -5,10 +5,7 @@
 
 namespace ResursBank\Module;
 
-use Resursbank\Ecommerce\Types\CheckoutType;
-use Resursbank\RBEcomPHP\ResursBank;
 use Resursbank\Woocommerce\Modules\Ordermanagement\Module as OrdermanagementModule;
-use TorneLIB\IO\Data\Arrays;
 
 /**
  * Class Plugin Internal plugin handler.
@@ -19,13 +16,6 @@ class PluginHooks
 {
     public function __construct()
     {
-        $this->getActions();
-    }
-
-    private function getActions(): void
-    {
-        add_action('woocommerce_order_refunded', [$this, 'refundResursOrder'], 10, 2);
-
         OrdermanagementModule::setupActions();
     }
 }
