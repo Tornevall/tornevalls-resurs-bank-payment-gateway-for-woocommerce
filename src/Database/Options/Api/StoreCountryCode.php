1c73c1d49522da36341d8d4246922da1337ae1e9
Tomas Tornevall <thorne@tornevall.net>
2024-02-19 11:02:33 +0100

    WOO-1285: Fixed cognitive complexity warning.

--- src/Database/Options/Api/StoreCountryCode.php
+++ src/Database/Options/Api/StoreCountryCode.php
@@ -9,12 +9,11 @@ declare(strict_types=1);
 
 namespace Resursbank\Woocommerce\Database\Options\Api;
 
-use Resursbank\Ecom\Config;
-use Resursbank\Ecom\Module\Store\Models\Store;
 use Resursbank\Ecom\Module\Store\Repository;
 use Resursbank\Woocommerce\Database\DataType\StringOption;
 use Resursbank\Woocommerce\Database\OptionInterface;
 use Resursbank\Woocommerce\Database\Options\Advanced\StoreId;
+use Resursbank\Woocommerce\Util\WooCommerce;
 use Throwable;
 
 /**
@@ -32,53 +31,55 @@ class StoreCountryCode extends StringOption implements OptionInterface
 
     /**
      * Returning a country code based on the store used in current config.
+     * @noinspection PhpArgumentWithoutNamedIdentifierInspection
      */
     public static function getCurrentStoreCountry(): string
     {
         $return = (string)get_transient('resurs_merchant_country_code');
 
-        try {
-            // If site is missing a proper initial setup, StoreId request will throw an exception.
-            // Also, if that request goes wrong it may do the same, so we will first verify that there are
-            // credentials available.
-            if (
-                !Config::hasInstance() ||
-                ClientId::getData() === '' &&
-                ClientSecret::getData() === ''
-            ) {
-                return $return;
-            }
-
-            $currentStoreId = StoreId::getData();
-        } catch (Throwable) {
-            // All other errors should be suppressed.
+        if (!WooCommerce::isValidSetup()) {
             return $return;
         }
 
-        // Failures from above should return pre-fetched data even if there is a chance that there is no data set.
+        $currentStoreId = self::getCurrentStoreId();
         if ($currentStoreId === '') {
             return $return;
         }
 
+        return self::getAndSetStoreCountryCode(currentStoreId: $currentStoreId, defaultReturn: $return);
+    }
+
+    /**
+     * @return string
+     */
+    private static function getCurrentStoreId(): string
+    {
+        try {
+            return StoreId::getData();
+        } catch (Throwable) {
+            return '';
+        }
+    }
+
+    /**
+     * @param string $currentStoreId
+     * @param string $defaultReturn
+     * @return string
+     * @noinspection PhpArgumentWithoutNamedIdentifierInspection
+     */
+    private static function getAndSetStoreCountryCode(string $currentStoreId, string $defaultReturn): string
+    {
         try {
             $storeList = Repository::getStores();
-
-            /** @var Store $store */
             foreach ($storeList->getData() as $store) {
                 if ($store->id === $currentStoreId) {
-                    /** @noinspection PhpArgumentWithoutNamedIdentifierInspection */
-                    set_transient(
-                        'resurs_merchant_country_code',
-                        $store->countryCode->value
-                    );
-                    $return = $store->countryCode->value;
-                    break;
+                    set_transient('resurs_merchant_country_code', $store->countryCode->value);
+                    return $store->countryCode->value;
                 }
             }
         } catch (Throwable) {
-            // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
+            // Ignore errors.
         }
-
-        return $return;
+        return $defaultReturn;
     }
 }
