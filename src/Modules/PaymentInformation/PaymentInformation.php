054e6f2e8e934d59a775109a706647140f4d8f6d
Tomas Tornevall <thorne@tornevall.net>
2024-03-25 10:50:32 +0100

    WOO-1296

--- src/Modules/PaymentInformation/PaymentInformation.php
+++ src/Modules/PaymentInformation/PaymentInformation.php
@@ -80,8 +80,17 @@ class PaymentInformation
         }
 
         echo '<style>' .
-            Sanitize::sanitizeHtml(html: EcomPaymentInformation::getCss()) .
+            Sanitize::sanitizeHtml(html: self::getCssLeftover()) .
             '</style>';
+    }
+
+    /**
+     * @return string
+     */
+    private static function getCssLeftover(): string
+    {
+        return ".rb-pi table tr:nth-child(even) {background-color: #006464;}
+.rb-pi table tr:nth-child(odd) {background-color: #009b96;}";
     }
 
     /**
