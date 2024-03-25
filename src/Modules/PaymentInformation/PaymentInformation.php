f46b0f6be7a061b32078e44b925517936beb214a
Tomas Tornevall <thorne@tornevall.net>
2024-03-25 11:01:18 +0100

    Duplicate methods

--- src/Modules/PaymentInformation/PaymentInformation.php
+++ src/Modules/PaymentInformation/PaymentInformation.php
@@ -85,15 +85,6 @@ class PaymentInformation
     }
 
     /**
-     * @return string
-     */
-    private static function getCssLeftover(): string
-    {
-        return ".rb-pi table tr:nth-child(even) {background-color: #006464;}
-.rb-pi table tr:nth-child(odd) {background-color: #009b96;}";
-    }
-
-    /**
      * Outputs the actual widget HTML
      */
     public function getWidget(): void
