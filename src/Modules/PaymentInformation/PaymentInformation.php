7c4e5c92e0ad460fed00e0674769bdd2a25d9465
Tomas Tornevall <thorne@tornevall.net>
2024-03-25 11:05:25 +0100

    Fixed docblock and method name.

--- src/Modules/PaymentInformation/PaymentInformation.php
+++ src/Modules/PaymentInformation/PaymentInformation.php
@@ -80,7 +80,7 @@ class PaymentInformation
         }
 
         echo '<style>' .
-            Sanitize::sanitizeHtml(html: self::getCssLeftover()) .
+            Sanitize::sanitizeHtml(html: self::getCss()) .
             '</style>';
     }
 
@@ -92,7 +92,10 @@ class PaymentInformation
         echo Sanitize::sanitizeHtml(html: $this->widget->content);
     }
 
-    private static function getCssLeftover(): string
+    /**
+     * Required by css styling for some unknown reason, to make the logo correctly placed.
+     */
+    private static function getCss(): string
     {
         return ".rb-pi table tr:nth-child(even) {background-color: #006464;}
 .rb-pi table tr:nth-child(odd) {background-color: #009b96;}";
