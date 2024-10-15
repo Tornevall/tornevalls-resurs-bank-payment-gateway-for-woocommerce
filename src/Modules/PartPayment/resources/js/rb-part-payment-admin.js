8c998be400bf33d5fd188e18fbbac5e0efc687fb
Thomas Tornevall <thomas.tornevall@resurs.se>
2024-10-15 14:40:52 +0200

    Minor stuff

--- src/Modules/PartPayment/resources/js/rb-part-payment-admin.js
+++ src/Modules/PartPayment/resources/js/rb-part-payment-admin.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function ($) {
     if (typeof Resursbank_GetPeriods !== 'undefined') {
         var dropDownPeriods = $('#resursbank_part_payment_period');
-
         dropDownPeriods.on('change', function () {
             if (dropDownPeriods.find('option').length > 1) {
                 $('#resursbank_part_payment_period').prop('disabled', false);
