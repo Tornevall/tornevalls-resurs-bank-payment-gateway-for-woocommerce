42f541df2a3dffebb8c4e36fd9f8a19c0b9eb09a
Thomas Tornevall <thomas.tornevall@resurs.se>
2024-10-15 14:24:15 +0200

    Handle disable elements better with the new conditions if necessary.

--- src/Modules/PartPayment/resources/js/rb-part-payment-admin.js
+++ src/Modules/PartPayment/resources/js/rb-part-payment-admin.js
@@ -1,12 +1,15 @@
 jQuery(document).ready(function ($) {
     if (typeof Resursbank_GetPeriods !== 'undefined') {
         var dropDownPeriods = $('#resursbank_part_payment_period');
-        if (dropDownPeriods.find('option').length > 1) {
-            $('#resursbank_part_payment_period').prop('disabled', false);
-        }
 
-        $('#resursbank_part_payment_payment_method').on('change', function () {
-            $('#resursbank_part_payment_period').prop('disabled', false);
-        });
-    };
+        dropDownPeriods.on('change', function () {
+            if (dropDownPeriods.find('option').length > 1) {
+                $('#resursbank_part_payment_period').prop('disabled', false);
+            }
+
+            $('#resursbank_part_payment_payment_method').on('change', function () {
+                $('#resursbank_part_payment_period').prop('disabled', false);
+            });
+        })
+    }
 });
