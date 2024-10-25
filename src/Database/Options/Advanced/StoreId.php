63797ece4b7a5bf2e56526c381f66f17c7bdce3a
Thomas Tornevall <thomas.tornevall@resurs.se>
2024-10-25 08:42:33 +0200

    Default values now returning empty instead of a store.

--- src/Database/Options/Advanced/StoreId.php
+++ src/Database/Options/Advanced/StoreId.php
@@ -9,14 +9,11 @@ declare(strict_types=1);
 
 namespace Resursbank\Woocommerce\Database\Options\Advanced;
 
-use Resursbank\Ecom\Module\Store\Repository;
 use Resursbank\Woocommerce\Database\DataType\StringOption;
 use Resursbank\Woocommerce\Database\OptionInterface;
 use Resursbank\Woocommerce\Database\Options\Api\ClientId;
 use Resursbank\Woocommerce\Database\Options\Api\ClientSecret;
 use Resursbank\Woocommerce\Settings\Api;
-use Resursbank\Woocommerce\Util\Log;
-use Throwable;
 
 /**
  * Implementation of resursbank_store_id value in options table.
@@ -57,22 +54,6 @@ class StoreId extends StringOption implements OptionInterface
             return '';
         }
 
-        $result = parent::getDefault();
-
-        try {
-            // This should ensure we have at least one store id available on request, if the merchant
-            // hasn't already configured this properly. If no data are stored already, the first available
-            // store will be returned. Just beware, since this is an early init request, we have to make sure
-            // that this is only fetched when there are proper and verified credentials set.
-            $collection = Repository::getStores();
-
-            if (count($collection) === 1) {
-                $result = $collection[0]->id;
-            }
-        } catch (Throwable $e) {
-            Log::error(error: $e);
-        }
-
-        return $result;
+        return parent::getDefault();
     }
 }
