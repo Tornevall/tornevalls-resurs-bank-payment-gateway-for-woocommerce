6331cb7ba9717321530f7014980c20da3b6315b0
Thomas Tornevall <thomas.tornevall@resurs.se>
2024-12-11 10:08:51 +0100

    As => Since

--- src/Modules/Gateway/Resursbank.php
+++ src/Modules/Gateway/Resursbank.php
@@ -96,7 +96,7 @@ class Resursbank extends WC_Payment_Gateway
 
         // When the blocks editor redirects admins to woocommerce internal sections
         // for handling payment methods, we need to redirect them back to the correct
-        // location as our methods are not editable from WooCommerce.
+        // location since our methods are not editable from WooCommerce.
         if ($this->id !== RESURSBANK_MODULE_PREFIX) {
             // Redirect to the correct section if the wrong section is requested when section is set to a method id.
             AdminUtility::redirectAtWrongSection(method: $method->id);
