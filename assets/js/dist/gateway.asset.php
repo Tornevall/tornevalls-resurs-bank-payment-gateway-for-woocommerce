b16b3e3e573bd9344fdd05d1d639c8fc2ba93c31
Thomas Tornevall <thomas.tornevall@resurs.se>
2025-01-27 13:20:18 +0100

    Build.

--- assets/js/dist/gateway.asset.php
+++ assets/js/dist/gateway.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data'), 'version' => '0bb7d9b142e6d74c124b');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data'), 'version' => '14a831525f7917398ac0');
