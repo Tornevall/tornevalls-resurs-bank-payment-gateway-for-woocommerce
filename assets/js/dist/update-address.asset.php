38a211480705a917dfcf32e0ce09cbcd6a5711e4
Thomas Tornevall <thomas.tornevall@resurs.se>
2025-02-04 15:05:49 +0100

    WOO-1415 + WOO-1416

--- assets/js/dist/update-address.asset.php
+++ assets/js/dist/update-address.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wc-blocks-data-store', 'wc-settings', 'wp-data'), 'version' => '6e70338d83da6d208d30');
+<?php return array('dependencies' => array('jquery', 'wc-blocks-data-store', 'wc-settings', 'wp-data'), 'version' => '5df978a9a8d5f7c85873');
