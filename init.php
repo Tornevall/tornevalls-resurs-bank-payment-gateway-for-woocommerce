abfc9d1bda3f1167019bfca430d241c0f0365f5f
Thomas Tornevall <thomas.tornevall@resurs.se>
2026-03-05 15:31:39 +0100

    method call renames.

--- init.php
+++ init.php
@@ -49,12 +49,12 @@ require_once __DIR__ . '/src/Autoloader/requirements.php';
 // checkout when ecom2 is missing. We cannot move this into a class, as the autoload
 // process will fail if ecom2 is unavailable.
 if (!file_exists(filename: __DIR__ . '/vendor/ecom/composer.json')) {
-    resursBankHasNoEcom();
+    resursbankabpaygw_has_no_ecom();
     return;
 }
 
 if (PHP_VERSION_ID < 80100) {
-    resursBankHasOldPhp();
+    resursbankabpaygw_has_old_php();
     return;
 }
 
